41aa09d88cb738a9b0af3e964ef28d4f7302c3d8
Alumina14 <fabiodigennaro14@gmail.com>
2021-01-30 23:00:04 +0100

    intial commit

---
+++ rent-car-luxury/src/components/Car.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Car() {
+    return (
+        <div>
+        car singola
+        </div>
+    )
+}
+
+export default Car
---
+++ rent-car-luxury/src/components/CarsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CarsList() {
+    return (
+        <div>
+            lista car
+        </div>
+    )
+}
+
+export default CarsList
